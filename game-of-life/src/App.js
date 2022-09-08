1226cef60ab9cfeb129a471b67eaa96ed1b0c348
Patrik Olsson <patrikolsson@Patriks-MacBook-Pro.local>
2022-09-08 10:15:36 +0100

    comment

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,6 +1,7 @@
 import './style.css';
 import Game from './Components/Game';
 
+//this will be occupying main space below banner when merged into full project
 function App() {
   return (
     <div className="App">
